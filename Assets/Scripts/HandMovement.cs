e876b3d716b469db4e9671958055a1fc83d05cf8
Kári Steinn Aðalsteinsson <kari_steinn@outlook.com>
2023-03-14 15:55:13 +0100

    Added text instructions to 6 weights and moved tables around

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -40,7 +40,10 @@ public class HandMovement : MonoBehaviour
         if (weightGrabbed)
         {
 
+            // Hand position with C/D ratio that that takes into account the velocity of the controller
             handPosition = initialPosition + Vector3.Scale(lastVelocity, new Vector3(1f, movementRatio, 1f)) * Time.deltaTime;
+            // Hand position with C/D ratio that that takes into account the position of the controller according to the formula used in https://ieeexplore.ieee.org/document/9669918
+            // handPosition = initialPosition + Vector3.Scale((controllerTransform.position - initialPosition), new Vector3(1.0f, movementRatio, 1.0f));
             initialPosition = handPosition;
             lastVelocity = velocityTrackerProcessor.GetVelocity();
         }
@@ -58,6 +61,14 @@ public class HandMovement : MonoBehaviour
             // The highest mass of 11 should have a movement ratio of 0.7 
             // and the lowest mass of 1 should have a ratio of 1.0
             movementRatio = 0.7f + 0.3f * (1f - ((mass - 1f) / 10f));
+            
+            // update the movement ratio based on the mass of the grabbed object. 
+            // The highest mass of 11 should have a movement ratio of 0.05 
+            // and the lowest mass of 1 should have a ratio of 0.3
+            // movementRatio = 0.05f + 0.25f * (1f - ((mass - 1f) / 10f));
+
+            // movementRatio = 0.05f + 0.95f * (1f - ((mass - 1f) / 10f));
+            Debug.Log("Weight grabbed with mass " + mass + " and movement ratio " + movementRatio);
         }
         initialPosition = controllerTransform.position;
         lastVelocity = velocityTrackerProcessor.GetVelocity();

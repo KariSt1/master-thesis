4ea292663f180c7012c5407456cf512293bfabb1
Kári Steinn Aðalsteinsson <kari_steinn@outlook.com>
2023-03-15 18:02:14 +0100

    Implemented new maxVelocity function and repositioned tables

--- Assets/Scripts/DropOnFastLift.cs
+++ Assets/Scripts/DropOnFastLift.cs
@@ -17,6 +17,9 @@ public class DropOnFastLift : MonoBehaviour
     private Vector3 startPosition;
     private bool isOutsideOriginLimit = false;
 
+    // TODO: Remove this, this is a toggle between my values for maxVelocity and the values from the paper
+    public bool usePaperValues = false;
+
     void Awake()
     {
         interactable = GetComponentInChildren<InteractableFacade>();
@@ -70,20 +73,24 @@ public class DropOnFastLift : MonoBehaviour
     }
 
     private void CalculateMaxVelocity() {
-        if (interactableRigidbody.mass > 9.83f) {
-              maxVelocity = (1.9f / interactableRigidbody.mass + 0.06f);
-        } else if (interactableRigidbody.mass > 2.36f) {
-            // speedLimit = ((2 / (interactableRigidbody.mass))+0.1f); works good
-            //speedLimit = ((1.5f / (interactableRigidbody.mass))+0.07f); // works good as well. A bit frustrating but I managed to get the first four scenes correct
-            //speedLimit = ((2.3f / (interactableRigidbody.mass))+0.07f); // used for user study1
-            //speedLimit = (4/(interactableRigidbody.mass+0.45f)-0.1f); //wip
-            // 4/(x+0.9)-0.2 //wip
-            //speedLimit = (1.9f / interactableRigidbody.mass + 0.06f); // prototype 3A
-            //speedLimit = (-0.08f * interactableRigidbody.mass + 1.1f); // combining linear
-            maxVelocity = (-0.08f * interactableRigidbody.mass + 1.05f); // combining linear
+        if (usePaperValues) {
+            if (interactableRigidbody.mass > 9.83f) {
+                maxVelocity = (1.9f / interactableRigidbody.mass + 0.06f);
+            } else if (interactableRigidbody.mass > 2.36f) {
+                // speedLimit = ((2 / (interactableRigidbody.mass))+0.1f); works good
+                //speedLimit = ((1.5f / (interactableRigidbody.mass))+0.07f); // works good as well. A bit frustrating but I managed to get the first four scenes correct
+                //speedLimit = ((2.3f / (interactableRigidbody.mass))+0.07f); // used for user study1
+                //speedLimit = (4/(interactableRigidbody.mass+0.45f)-0.1f); //wip
+                // 4/(x+0.9)-0.2 //wip
+                //speedLimit = (1.9f / interactableRigidbody.mass + 0.06f); // prototype 3A
+                //speedLimit = (-0.08f * interactableRigidbody.mass + 1.1f); // combining linear
+                maxVelocity = (-0.08f * interactableRigidbody.mass + 1.05f); // combining linear
+            } else {
+                maxVelocity = (-0.8f * interactableRigidbody.mass + 2.75f); // prototype 3A or cobining linear
+                //speedLimit = (-1.8f * interactableRigidbody.mass + 3.6f);
+            }
         } else {
-            maxVelocity = (-0.8f * interactableRigidbody.mass + 2.75f); // prototype 3A or cobining linear
-            //speedLimit = (-1.8f * interactableRigidbody.mass + 3.6f);
+            maxVelocity = (1/(Mathf.Log(interactableRigidbody.mass + 1, 10))) - 0.5f;
         }
         Debug.Log("max velocity: " + maxVelocity);
     }

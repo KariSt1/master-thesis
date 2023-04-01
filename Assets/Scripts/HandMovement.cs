0d17364cf3b893b6fd74932dbb57d2caadf64540
Kári Steinn Aðalsteinsson <kari_steinn@outlook.com>
2023-04-01 17:22:04 +0200

    Added milk carton model and changed CD ratio functionality to stop it from lagging

--- Assets/Scripts/HandMovement.cs
+++ Assets/Scripts/HandMovement.cs
@@ -30,25 +30,49 @@ public class HandMovement : MonoBehaviour
 
     private bool weightGrabbed = false;
 
+    // Test variables
+    private Vector3 controllerInitialPosition;
+    private Vector3 handInitialPosition;
+
     // Set in the Modified EventData of the Position ProperyModifer in Internal on the tracked alias
     public void UpdateLocation()
     {
         if (CDRatioEnabled && weightGrabbed)
         {
+            Debug.Log("TESTING: Updating hand position");
             handTransform.position = handPosition;
+            Debug.Log("Hand transform position in UpdateLocation: " + handPosition);
+            Debug.Log("Controller transform position in UpdateLocation: " + controllerTransform.position);
         }
     }
-    void Update()
+    void LateUpdate()
     {
         if (CDRatioEnabled && weightGrabbed)
         {
+            /** OLD WAY
+            Debug.Log("TESTING: In Update");
             // Debug.Log("In update weight grabbed");
             // Hand position with C/D ratio that that takes into account the velocity of the controller
+            Debug.Log("Hand position before changing: " + handPosition);
+            Debug.Log("Handtransform position: " + handTransform.position);
+            lastVelocity = velocityTrackerProcessor.GetVelocity();
             handPosition = initialPosition + Vector3.Scale(lastVelocity, new Vector3(1f, movementRatio, 1f)) * Time.deltaTime;
+            Debug.Log("Hand position after changing: " + handPosition);
             // Hand position with C/D ratio that that takes into account the position of the controller according to the formula used in https://ieeexplore.ieee.org/document/9669918
             // handPosition = initialPosition + Vector3.Scale((controllerTransform.position - initialPosition), new Vector3(1.0f, movementRatio, 1.0f));
             initialPosition = handPosition;
             lastVelocity = velocityTrackerProcessor.GetVelocity();
+            **/
+            // new position s2 = s1 + v_avg * t
+            // where s2 is the new position, s1 is the old position, v_avg is the average velocity, and t is the time
+            
+            
+            // lastVelocity = velocityTrackerProcessor.GetVelocity();
+            // Vector3 s2 = initialPosition + lastVelocity * Time.deltaTime;
+            // handPosition = Vector3.Scale((s2 - initialPosition), new Vector3(1f, movementRatio, 1f)) + initialPosition;
+            // initialPosition = handPosition;
+
+            handPosition = Vector3.Scale((controllerTransform.position - initialPosition), new Vector3(1f, movementRatio, 1f)) + initialPosition;
         }
     }
 
@@ -78,6 +102,9 @@ public class HandMovement : MonoBehaviour
 
                 // movementRatio = 0.05f + 0.95f * (1f - ((mass - 1f) / 10f));
                 Debug.Log("Weight grabbed with mass " + mass + " and movement ratio " + movementRatio);
+
+                controllerInitialPosition = controllerTransform.position;
+                handInitialPosition = handTransform.position;
             }
             initialPosition = controllerTransform.position;
             lastVelocity = velocityTrackerProcessor.GetVelocity();
@@ -90,5 +117,12 @@ public class HandMovement : MonoBehaviour
     {
         Debug.Log("Weight ungrabbed");
         weightGrabbed = false;
+        Debug.Log("Controller initial position: " + controllerInitialPosition);
+        Debug.Log("Hand initial position: " + handInitialPosition);
+        Debug.Log("Controller final position: " + controllerTransform.position);
+        Debug.Log("Hand final position: " + handTransform.position);
+        Debug.Log("Ratio of controller movement to hand movement: " + (handTransform.position.y - handInitialPosition.y) / (controllerTransform.position.y - controllerInitialPosition.y));
+        Debug.Log("Total controller movement: " + (controllerTransform.position.y - controllerInitialPosition.y));
+        Debug.Log("Total hand movement: " + (handTransform.position.y - handInitialPosition.y));
     }
 }
